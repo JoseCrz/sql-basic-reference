b31aa0f6c94bcbaa2403d0be7904ec53c58b0864
Jose Cuevas <josecueram@gmail.com>
2020-04-02 10:56:08 -0600

    added left join examples

--- commands/join.sql
+++ commands/join.sql
@@ -56,3 +56,51 @@ SELECT c.name AS client, b.title, a.name AS author, t.type
 -- | Maria Dolores Gomez | Estudio en escarlata                 | Arthur Conan Doyle | lend |
 -- | Maria Luisa Marin   | Fundamentals of Wavelets             | Jaideva Goswami    | lend |
 -- +---------------------+--------------------------------------+--------------------+------+
+
+-- ! LEFT JOIN
+-- * Basic left join
+SELECT a.author_id, a.name, a.nationality, b.title
+FROM authors as a
+LEFT JOIN books as b
+    ON a.author_id = b.author_id
+WHERE a.author_id BETWEEN 1 AND 5;
+
+-- +-----------+--------------------+-------------+---------------------------------------+
+-- | author_id | name               | nationality | title                                 |
+-- +-----------+--------------------+-------------+---------------------------------------+
+-- |         1 | Sam Altman         | USA         | The Startup Playbook                  |
+-- |         1 | Sam Altman         | USA         | The Startup Playbook                  |
+-- |         3 | Arthur Conan Doyle | GBR         | Estudio en escarlata                  |
+-- |         5 | Juan Rulfo         | MEX         | El llano en llamas                    |
+-- |         3 | Arthur Conan Doyle | GBR         | The - Vol I Complete Sherlock Holmes  |
+-- |         3 | Arthur Conan Doyle | GBR         | The - Vol II Complete Sherlock Holmes |
+-- |         2 | Freddy Vega        | COL         | NULL                                  |
+-- |         4 | Chuck Palahniuk    | USA         | NULL                                  |
+-- +-----------+--------------------+-------------+---------------------------------------+
+
+-- * LEFT JOIN with GROUP BY and ORDER BY
+SELECT a.author_id, a.name, a.nationality, COUNT(b.book_id) AS number_of_books
+FROM authors as a
+LEFT JOIN books as b
+    ON a.author_id = b.author_id
+WHERE a.author_id BETWEEN 1 AND 5
+GROUP BY a.author_id
+ORDER BY a.author_id ASC;
+
+-- -- +-----------+--------------------+-------------+-----------------+
+-- | author_id | name               | nationality | number_of_books |
+-- +-----------+--------------------+-------------+-----------------+
+-- |         1 | Sam Altman         | USA         |               2 |
+-- |         2 | Freddy Vega        | COL         |               0 |
+-- |         3 | Arthur Conan Doyle | GBR         |               3 |
+-- |         4 | Chuck Palahniuk    | USA         |               0 |
+-- |         5 | Juan Rulfo         | MEX         |               1 |
+-- +-----------+--------------------+-------------+-----------------+
+
+SELECT a.author_id, a.name, a.nationality, COUNT(b.book_id) AS number_of_books
+FROM authors as a
+RIGHT JOIN books as b
+    ON a.author_id = b.author_id
+WHERE a.author_id BETWEEN 1 AND 5
+GROUP BY a.author_id
+ORDER BY a.author_id ASC;
