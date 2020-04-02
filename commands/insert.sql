d9cd0c15385a21b7a659aed04a2e66ee71818078
Jose Cuevas <josecueram@gmail.com>
2020-04-01 20:32:44 -0600

    added another example of a more advance insert query

--- commands/insert.sql
+++ commands/insert.sql
@@ -12,3 +12,8 @@ INSERT INTO authors(name, nationality) VALUES('Octavio Paz', "MEX"), ('Julio Cor
 -- * Insert row when unique value already in table, sets old value to new one
 INSERT INTO clients(name, email, birthdate, gender, active) VALUES('Phil Phish', 'phil@fez.com', '1989-09-01','M',0)
 ON DUPLICATE KEY UPDATE active = VALUES(active);
+
+-- * Insert with nested select: used to insert a value retrieved from another database
+-- thread carefully as this type of queries can't consume many resources
+INSERT INTO books(title, `year`, author_id) 
+VALUES('El llano en llamas', 1956, (SELECT author_id FROM authors WHERE name = 'Juan Rulfo'));
